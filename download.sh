5852fed842fe7045ddd1fe527f8b4a4ccf4cd2e8
Yedid <yedid.hoshen@gmail.com>
2019-02-12 09:04:08 +0200

    Updated dictionary links

--- download.sh
+++ download.sh
@@ -4,5 +4,5 @@
 mkdir data
 wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.$1.vec -P data
 wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.$2.vec -P data
-wget https://s3.amazonaws.com/arrival/dictionaries/$1-$2.5000-6500.txt -P data
-wget https://s3.amazonaws.com/arrival/dictionaries/$2-$1.5000-6500.txt -P data
+wget https://dl.fbaipublicfiles.com/arrival/dictionaries/$1-$2.5000-6500.txt -P data
+wget https://dl.fbaipublicfiles.com/arrival/dictionaries/$2-$1.5000-6500.txt -P data
