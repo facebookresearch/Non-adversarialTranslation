6a5a51bc6445faed55df6c4c531779f652d8016e
Lior Wolf <liorwolf@gmail.com>
2020-04-02 18:06:20 +0300

    updating the links
    removing obsolete links

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 #!/bin/sh
 mkdir data
-wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.$1.vec -P data
-wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.$2.vec -P data
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.$1.zip -P data
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.$2.zip -P data
 wget https://dl.fbaipublicfiles.com/arrival/dictionaries/$1-$2.5000-6500.txt -P data
 wget https://dl.fbaipublicfiles.com/arrival/dictionaries/$2-$1.5000-6500.txt -P data
